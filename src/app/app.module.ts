522fb64c1beaab217948f09ee1247afab778275c
Diana Sodji <d.sodji@marinom.de>
2023-02-14 15:03:21 +0100

    authentification erstellt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import {NgModule} from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
+
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { LoginComponent } from './login/login.component';
+
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular/public_api';
+import { initializeKeycloak } from './utility/app.init';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,18 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
