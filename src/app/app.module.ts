8f70b52b12246b2c8bd210b1e1931c23eaacb21e
Diana Sodji <d.sodji@marinom.de>
2023-02-14 16:30:41 +0100

    Verbindung to docker hergestellt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
-
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { LoginComponent } from './login/login.component';
-
-import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular/public_api';
-import { initializeKeycloak } from './utility/app.init';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { initializeKeycloak } from '../app/utility/app.init';
 
 @NgModule({
   declarations: [
