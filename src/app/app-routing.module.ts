d9e1f53c22e57f8995046776464f3fc1dc7c9c62
Diana Sodji <d.sodji@marinom.de>
2023-02-15 18:24:54 +0100

    update für Mitarbeiter ddetails

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import{EmployeeListComponent} from "./employee-list/employee-list.component";
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from '../app/utility/app.guard';
+import{EmployeeDetailsComponent} from "./employee-details/EmployeeDetailsComponent";
 
 const routes: Routes = [
   { path: '', redirectTo: '/employee', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
+  { path: 'details', component: EmployeeDetailsComponent},
   { path: 'employee', component: EmployeeListComponent, canActivate: [AuthGuard]}
   
   
